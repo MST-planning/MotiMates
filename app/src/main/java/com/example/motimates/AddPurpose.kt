e5d8c56b8b77b6f72a4cde543d2c28022dfdba09
choiminsun <backwonand@sookmyung.ac.kr>
2023-11-24 14:29:55 +0900

    add button xml 작성 완료

---
+++ app/src/main/java/com/example/motimates/AddPurpose.kt
@@ -0,0 +1,13 @@
+package com.example.motimates
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPurpose : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_purpose)
+
+
+    }
+}
