d930e4c92ce3e250a78e744be49123c002e848a0
newoceanwave <leeseohyun02@naver.com>
2023-11-30 16:26:33 +0900

    [feat] 마이페이지 xml 추가(#9)

--- app/src/main/java/com/example/motimates/MyPageActivity.kt
+++ app/src/main/java/com/example/motimates/MyPageActivity.kt
@@ -26,7 +26,7 @@ class MyPageActivity : AppCompatActivity() {
         // 목표 추가하기 버튼 클릭 이벤트
         binding.addGoalButton.setOnClickListener {
             // 목표 추가하기 버튼 클릭 시 처리
-            val intent = Intent(this, Achievement::class.java)
+            val intent = Intent(this, AddPurpose::class.java)
             startActivity(intent)
         }
 
