5e0a3ba815c8381843095f98f127ac8e73642e13
choiminsun <backwonand@sookmyung.ac.kr>
2023-11-30 12:12:08 +0900

    알림 퍼미션, 스크립트

--- app/src/main/java/com/example/motimates/AddPurpose.kt
+++ app/src/main/java/com/example/motimates/AddPurpose.kt
@@ -1,15 +1,20 @@
 package com.example.motimates
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.app.NotificationCompat
 import com.example.motimates.databinding.AddPurposeBinding
 import java.util.Calendar
 
 class AddPurpose : AppCompatActivity() {
-
+    //목표 종료 일자
     private var calendar = Calendar.getInstance()
+    //해당 목표에 대한 알림 시간 리스트
+    private var times = mutableListOf<List<Int>>()
     var datetext = "string"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,11 +47,29 @@ class AddPurpose : AppCompatActivity() {
                     binding.customText.text = "${datetext}까지"
                 }
             }
+                                                    }
+
+        binding.save.setOnClickListener{
+            val intent = Intent(this, MyPageActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.addTime.setOnClickListener{
+            //화면에 timePicker 생성
+            TimePickerDialog(this, time, 12, 0,true).show()
+
+            //선택한 시간 목록 보여주기
         }
     }
 
     private val date = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
         calendar.set(year, month, dayOfMonth)
         datetext = "${calendar.get(Calendar.YEAR)}년 ${calendar.get(Calendar.MONTH)}월 ${calendar.get(Calendar.DATE)}일까지"
+    }
+
+    private val time = TimePickerDialog.OnTimeSetListener{ view, hour, minute ->
+        val t = listOf<Int>(hour, minute)
+
+        times.add(t)
     }
 }
