2ea52c65dfb4a413ca69148d3cd00d625abb0167
sje21 <lololog@naver.com>
2023-12-06 21:33:21 +0900

    acievement, myflower, mygarden 툴바-네비게이션바 추가

--- app/src/main/java/com/example/motimates/MyGarden.kt
+++ app/src/main/java/com/example/motimates/MyGarden.kt
@@ -1,11 +1,28 @@
 package com.example.motimates
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MyGarden : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_garden)
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer)
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+
+        setSupportActionBar(toolbar)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 }
